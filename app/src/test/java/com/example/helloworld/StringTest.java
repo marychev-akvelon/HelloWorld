dfd40d4f1e5dc5251ee97c48946dce8ff84e90f4
Mikhail Marychev <v-mimary@microsoft.com>
2020-03-28 05:12:18 +0300

    check fail test string*

---
+++ app/src/test/java/com/example/helloworld/StringTest.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class StringTest {
+    @Test
+    public void string_CorrectEmailSimple_ReturnsTrue() {
+        assertEquals(4, 2 + 2);
+    }
+}
